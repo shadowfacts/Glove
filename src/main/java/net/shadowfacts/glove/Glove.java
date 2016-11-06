abcee97a1cb98a115392da011e3fb3061317028d
Shadowfacts <me@shadowfacts.net>
2016-11-06 14:57:57 -0500

    Initial commit

---
+++ src/main/java/net/shadowfacts/glove/Glove.java
@@ -0,0 +1,102 @@
+package net.shadowfacts.glove;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumHandSide;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.event.RenderSpecificHandEvent;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+import net.shadowfacts.shadowmc.ShadowMC;
+import net.shadowfacts.shadowmc.item.ItemBase;
+import org.lwjgl.input.Keyboard;
+
+/**
+ * @author shadowfacts
+ */
+@Mod(modid = Glove.MODID, name = Glove.NAME, version = Glove.VERSION, dependencies = "required-after:shadowmc@[3.4.5,);", acceptedMinecraftVersions = "[1.10.2]")
+public class Glove {
+
+	public static final String MODID = "glove";
+	public static final String NAME = "Glove";
+	public static final String VERSION = "@VERSION@";
+
+	private static final int MSG_ID = 85663;
+
+	public static KeyBinding keyBinding = new KeyBinding("glove:mode.key", KeyConflictContext.IN_GAME, KeyModifier.NONE, Keyboard.KEY_G, MODID);
+	public static boolean gloveMode = false;
+
+//	Content
+	public static ItemBase glove = new ItemBase("glove");
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		GameRegistry.register(glove);
+//		GameRegistry.addRecipe(new ShapelessOreRecipe());
+	}
+
+	@Mod.EventHandler
+	@SideOnly(Side.CLIENT)
+	public void preInitClient(FMLPreInitializationEvent event) {
+		glove.initItemModel();
+		MinecraftForge.EVENT_BUS.register(this);
+
+		ClientRegistry.registerKeyBinding(keyBinding);
+	}
+
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+		if (keyBinding.isPressed()) {
+			gloveMode = !gloveMode;
+			ShadowMC.proxy.sendSpamlessMessage(Minecraft.getMinecraft().thePlayer, new TextComponentTranslation(gloveMode ? "glove:mode.enable" : "glove:mode.disable"), MSG_ID);
+		}
+	}
+
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onModelBake(ModelBakeEvent event) {
+		IBakedModel original = event.getModelRegistry().getObject(new ModelResourceLocation("glove:glove", "inventory"));
+		event.getModelRegistry().putObject(new ModelResourceLocation("glove:glove", "inventory"), new GloveModel(original));
+	}
+
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onRenderSpecificHand(RenderSpecificHandEvent event) {
+		if (gloveMode || (event.getItemStack() != null && event.getItemStack().getItem() == glove)) {
+			event.setCanceled(true);
+
+			EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
+
+			if (player.isInvisible()) return;
+
+			float partialTicks = event.getPartialTicks();
+			float f = player.getSwingProgress(partialTicks);
+			float f1 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * partialTicks;
+			float f3 = event.getHand() == EnumHand.MAIN_HAND ? f : 0.0F;
+			ItemRenderer renderer = Minecraft.getMinecraft().getItemRenderer();
+			float f5 = 1.0F - (renderer.prevEquippedProgressMainHand + (renderer.equippedProgressMainHand - renderer.prevEquippedProgressMainHand) * partialTicks);
+			renderer.renderItemInFirstPerson(player, partialTicks, f1, EnumHand.MAIN_HAND, f3, null, f5);
+		}
+	}
+
+}
---
+++ src/main/java/net/shadowfacts/glove/GloveModel.java
@@ -0,0 +1,73 @@
+package net.shadowfacts.glove;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.*;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.client.model.*;
+import org.apache.commons.lang3.tuple.Pair;
+
+import javax.annotation.Nullable;
+import javax.vecmath.Matrix4f;
+import java.util.List;
+
+/**
+ * @author shadowfacts
+ */
+public class GloveModel implements IPerspectiveAwareModel {
+
+	private IBakedModel original;
+
+	public GloveModel(IBakedModel original) {
+		this.original = original;
+	}
+
+	@Override
+	public Pair<? extends IBakedModel, Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType) {
+		Matrix4f matrix = new Matrix4f();
+		matrix.setIdentity();
+		switch (cameraTransformType) {
+			case GUI:
+				return Pair.of(original, matrix);
+			default:
+				return Pair.of(this, matrix);
+		}
+	}
+
+	@Override
+	public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand) {
+		return ImmutableList.of();
+	}
+
+	@Override
+	public boolean isAmbientOcclusion() {
+		return false;
+	}
+
+	@Override
+	public boolean isGui3d() {
+		return false;
+	}
+
+	@Override
+	public boolean isBuiltInRenderer() {
+		return false;
+	}
+
+	@Override
+	public TextureAtlasSprite getParticleTexture() {
+		return null;
+	}
+
+	@Override
+	public ItemCameraTransforms getItemCameraTransforms() {
+		return ItemCameraTransforms.DEFAULT;
+	}
+
+	@Override
+	public ItemOverrideList getOverrides() {
+		return ItemOverrideList.NONE;
+	}
+
+}
